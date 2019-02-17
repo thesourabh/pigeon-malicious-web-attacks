7d04f6b14e7d9f60f95d7ab2b40c52d80fca1e44
Hassan Nadeem <hnadeem@vt.edu>
2019-02-17 15:02:25 -0500

    add instruction, working for python 2.7

---
+++ README.md
@@ -0,0 +1,7 @@
+`sudo su`
+`apt-get install sqlite3 libsqlite3-dev`
+`pip install -r requirements.txt`
+
+
+`python manage.py start app simplesocial`
+`python manage.py start`
--- simplesocial/views.py
+++ simplesocial/views.py
@@ -1,3 +1,11 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import (
+         bytes, dict, int, list, object, range, str,
+         ascii, chr, hex, input, next, oct, open,
+         pow, round, super,
+         filter, map, zip)
+
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
