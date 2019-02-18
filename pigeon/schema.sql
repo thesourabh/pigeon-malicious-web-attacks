b9434befd9393c2cb80aa6bfac7c28fb9b3d782b
Hassan Nadeem <hnadeem@vt.edu>
2019-02-18 13:43:10 -0500

    Change name to pigeon

--- pigeon/schema.sql
+++ pigeon/schema.sql
@@ -32,7 +32,15 @@ INSERT INTO user values(1, 'admin', 'admin', 'Admin', 'Admin');
 INSERT INTO user values(2, 'hnadeem', 'hnadeem', 'Hassan', 'Nadeem');
 INSERT INTO user values(3, 'Pranavi', 'Pranavi', 'Pranavi', 'Rambhakta');
 INSERT INTO user values(4, 'Sourabh', 'Sourabh', 'Sourabh', 'Shetty');
+INSERT INTO user values(5, 'Lonely', 'Lonely', 'Lonely', 'Misanthrope');
 
 INSERT INTO post values(NULL, 3, '2020-11-11 13:23:44', 'OMG, Hassan is so cool!');
 INSERT INTO post values(NULL, 4, '2015-07-07 13:23:44', "A huge round of applause for what I can only describe as Eva Green's rap battle with Lucifer #PennyDreadful.");
 INSERT INTO post values(NULL, 4, '2017-08-08 13:23:44', "PSA: Before y'all start shipping them, friendly reminder that Jon Snow is Daenerys' nephew. #GameOfThrones");
+
+INSERT INTO relation values(NULL, 2, 3, 1);
+INSERT INTO relation values(NULL, 2, 4, 1);
+INSERT INTO relation values(NULL, 3, 2, 1);
+INSERT INTO relation values(NULL, 3, 4, 1);
+INSERT INTO relation values(NULL, 4, 2, 1);
+INSERT INTO relation values(NULL, 4, 3, 1);
